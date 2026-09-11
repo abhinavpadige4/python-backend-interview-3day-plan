77a4e027fb7e65d7b295531265dae353f94b6326
abhinav <abhinavpadige06@gmail.com>
2026-09-12 00:47:56 +0530

    Add Day 2 SQL solutions

---
+++ solutions/day2/015_delete_duplicate_emails.sql
@@ -0,0 +1,13 @@
+"""
+196 Delete Duplicate Emails
+https://leetcode.com/problems/delete-duplicate-emails/
+
+Write a SQL query to delete all duplicate email entries in a table named Person, 
+keeping only unique emails based on its smallest Id.
+"""
+
+DELETE p1 FROM Person p1
+INNER JOIN Person p2 
+WHERE 
+    p1.Email = p2.Email AND
+    p1.Id > p2.Id;

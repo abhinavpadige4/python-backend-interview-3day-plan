0bcee25ef50a5e82e45dd80b9111e6c98463bb5b
abhinav <abhinavpadige06@gmail.com>
2026-09-12 00:47:05 +0530

    Add Day 1 SQL solutions

---
+++ solutions/day1/009_consecutive_numbers.sql
@@ -0,0 +1,18 @@
+"""
+180 Consecutive Numbers
+https://leetcode.com/problems/consecutive-numbers/
+
+Write a SQL query to find all numbers that appear at least three times consecutively.
+"""
+
+SELECT DISTINCT
+    l1.Num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num;
